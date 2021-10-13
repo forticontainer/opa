1bc1beed09b62f1efd7b6c4f700f1e7b396eff2a
Yang Chen <cheny@fortinet.com>
2021-10-13 10:31:29 -0700

    shell with dir and port
    Signed-off-by: Yang Chen <cheny@fortinet.com>

--- run_opa.sh
+++ run_opa.sh
@@ -1,10 +1,29 @@
+#!/bin/bash
+
+# setup OPA directory and check OPA executable
+if [ -z "$2" ]
+then
+    opa_dir=/opt/fortics/fcs-worker
+else
+    opa_dir=$2
+fi
+
+opa_executable=$opa_dir/opa
+if [ -e opa_executable ]
+then
+    echo "cannot locate opa executable from $opa_executable"
+    exit 1
+fi
+
+# run OPA
 if [ -z "$1" ]
 then
     echo "running OPA on default port"
-    ./opa run --server > /dev/null 2>&1 &
+    $opa_executable run --server > /dev/null 2>&1 &
 else
     echo "running OPA on port $1"
-    ./opa run --server --addr $1 > /dev/null 2>&1 &
+    $opa_executable --server --addr $1 > /dev/null 2>&1 &
 fi
-# might check port with "nc"
+
+# might check OPA port with "nc" if available
 # nc -z 127.0.0.1 $1
