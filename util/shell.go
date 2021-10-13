20f879746069857e438a7d7beffd6c9998c9453c
Yang Chen <cheny@fortinet.com>
2021-10-13 10:32:54 -0700

    remove comment
    Signed-off-by: Yang Chen <cheny@fortinet.com>

--- util/shell.go
+++ util/shell.go
@@ -21,9 +21,5 @@ func ExecuteShell(command string) (string, error) {
 		return errOut.String(), err
 	}
 
-	// if errOut buffer is not empty, then it means something is wrong
-	//if errOut.Len() != 0 {
-	//	return "", errors.New(errOut.String())
-	//}
 	return out.String(), nil
 }
